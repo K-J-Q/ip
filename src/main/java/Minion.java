5b3f38d871d28f148ba525840e72264ca11ef067
JQ <71882072+K-J-Q@users.noreply.github.com>
2025-01-20 23:19:40 +0800

    Rename Duke to Minion

---
+++ src/main/java/Minion.java
@@ -0,0 +1,10 @@
+public class Minion {
+    public static void main(String[] args) {
+        String logo = "  __  __ _       _             \n"
+            + " |  \\/  (_)_ __ (_) ___  _ __  \n"
+            + " | |\\/| | | '_ \\| |/ _ \\| '_ \\ \n"
+            + " | |  | | | | | | | (_) | | | |\n"
+                + " |_|  |_|_|_| |_|_|\\___/|_| |_|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
