15d4a252b0568ef9ba6f19b05b57be6e02806dc8
JQ <71882072+K-J-Q@users.noreply.github.com>
2025-01-20 23:20:31 +0800

    Add Increment 0 - greets the user and exits

--- src/main/java/Minion.java
+++ src/main/java/Minion.java
@@ -5,6 +5,16 @@ public class Minion {
             + " | |\\/| | | '_ \\| |/ _ \\| '_ \\ \n"
             + " | |  | | | | | | | (_) | | | |\n"
                 + " |_|  |_|_|_| |_|_|\\___/|_| |_|\n";
+        
+        String lineSeperator = "____________________________________________________________";
+        String name = "Minion";
+
         System.out.println("Hello from\n" + logo);
+        System.out.println(lineSeperator);
+        System.out.println("Hello! I'm [" + name + "]");
+        System.out.println("What can I do for you?");
+        System.out.println(lineSeperator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(lineSeperator);
     }
 }
