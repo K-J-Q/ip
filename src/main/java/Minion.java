abeb0dd9a7662a6506d6bbad52f5b0b5af3565ce
JQ <71882072+K-J-Q@users.noreply.github.com>
2025-01-22 13:31:42 +0800

    Fix spelling and add comments

--- src/main/java/Minion.java
+++ src/main/java/Minion.java
@@ -1,20 +1,21 @@
 public class Minion {
     public static void main(String[] args) {
+        // Constant variables
         String logo = "  __  __ _       _             \n"
             + " |  \\/  (_)_ __ (_) ___  _ __  \n"
             + " | |\\/| | | '_ \\| |/ _ \\| '_ \\ \n"
             + " | |  | | | | | | | (_) | | | |\n"
                 + " |_|  |_|_|_| |_|_|\\___/|_| |_|\n";
-        
-        String lineSeperator = "____________________________________________________________";
+        String lineSeparator = "____________________________________________________________";
         String name = "Minion";
 
+        // Display logo and messages
         System.out.println("Hello from\n" + logo);
-        System.out.println(lineSeperator);
+        System.out.println(lineSeparator);
         System.out.println("Hello! I'm [" + name + "]");
         System.out.println("What can I do for you?");
-        System.out.println(lineSeperator);
+        System.out.println(lineSeparator);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(lineSeperator);
+        System.out.println(lineSeparator);
     }
 }
