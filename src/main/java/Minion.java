8f1d9359402851b4fda2d21a592abca9ae92dac4
JQ <71882072+K-J-Q@users.noreply.github.com>
2025-01-25 22:49:02 +0800

    Add indentation for Minion output messages

--- src/main/java/Minion.java
+++ src/main/java/Minion.java
@@ -6,16 +6,17 @@ public class Minion {
             + " | |\\/| | | '_ \\| |/ _ \\| '_ \\ \n"
             + " | |  | | | | | | | (_) | | | |\n"
                 + " |_|  |_|_|_| |_|_|\\___/|_| |_|\n";
-        String lineSeparator = "____________________________________________________________";
+        String indent = "    ";
+        String lineSeparator = indent + "____________________________________________________________";
         String name = "Minion";
 
         // Display logo and messages
-        System.out.println("Hello from\n" + logo);
+        System.out.println(indent + "Hello from\n" + logo);
         System.out.println(lineSeparator);
-        System.out.println("Hello! I'm [" + name + "]");
-        System.out.println("What can I do for you?");
+        System.out.println(indent + "Hello! I'm [" + name + "]");
+        System.out.println(indent + "What can I do for you?");
         System.out.println(lineSeparator);
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(indent + "Bye. Hope to see you again soon!");
         System.out.println(lineSeparator);
     }
 }
