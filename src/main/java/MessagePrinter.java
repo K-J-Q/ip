e5505104f6f1cfd4b331317d3e9fcf2ef38bc1bd
JQ <71882072+K-J-Q@users.noreply.github.com>
2025-01-26 12:47:35 +0800

    Comply with coding standard

--- src/main/java/MessagePrinter.java
+++ src/main/java/MessagePrinter.java
@@ -4,21 +4,21 @@ public class MessagePrinter {
     private String lineSeparator = "____________________________________________________________";
 
     // adds an indentation of 4 spaces "    "
-    private String addIndentation(String message){
+    private String addIndentation(String message) {
         // add indentation after every new line too
         message = message.replace("\n", "\n" + indentation);
         return this.indentation + message;
     }
 
-    public void printSep(){
+    public void printSep() {
         System.out.println(addIndentation(lineSeparator));
     }
 
-    public void printMessage(String message){
+    public void printMessage(String message) {
         System.out.println(addIndentation(message));
     }
 
-    public void printMessageAndSep(String message){
+    public void printMessageAndSep(String message) {
         this.printSep();
         this.printMessage(message);
         this.printSep();
